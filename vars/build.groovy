88b1a3c67713e89fbc61e28931d27a7528fbb352
RajagopalK <35064459+RajagopalK@users.noreply.github.com>
2018-12-14 17:16:02 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -10,12 +10,12 @@ def call(body) {
     def msg = pipelineParams.get("msg", "Hello World")
 if(msg.equals("Gradle")) {
     stage('GRADLE_BUILD') {
-     echo "${msg}"
+     gradle clean build
     }
    }
 else if(msg.equals("Maven")) {
     stage('MAVEN_BUILD') {
-     echo "${msg}"
+     mvn clean install
     }
    }
 }
