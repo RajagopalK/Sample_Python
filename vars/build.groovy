ea3270e7718aee5b75a8e81cbf53905911b2241d
RajagopalK <35064459+RajagopalK@users.noreply.github.com>
2018-12-17 12:20:33 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,27 +1,27 @@
 #!/usr/bin/groovy
-def call(body) {
-	node {
-			// evaluate the body block, and collect configuration into the object
-			// configParameters(artifactType,artifactTypeZip)
-			def pipelineParams = [:]
-			body.resolveStrategy = Closure.DELEGATE_FIRST
-			body.delegate = pipelineParams
-			body()
+node {
+	def call(body) {
+		// evaluate the body block, and collect configuration into the object
+		// configParameters(artifactType,artifactTypeZip)
+		def pipelineParams = [:]
+		body.resolveStrategy = Closure.DELEGATE_FIRST
+		body.delegate = pipelineParams
+		body()
+	
+		def msg = pipelineParams.get("msg", "Hello World")
+		try{
+	if(msg.equals("Gradle")) {
+		stage('GRADLE_BUILD') {
 		
-			def msg = pipelineParams.get("msg", "Hello World")
-			try{
-		if(msg.equals("Gradle")) {
-			stage('GRADLE_BUILD') {
-			
-				bat 'gradle clean build'
-		
-			}
+			bat 'gradle clean build'
+	
 		}
-		else if(msg.equals("Maven")) {
-			stage('MAVEN_BUILD') {
-					bat 'mvn -f C:\Users\670334\.jenkins\workspace\pipeline-project\pom.xml clean install'
-			}
-		} catch(Exception e) {
+	}
+	else if(msg.equals("Maven")) {
+		stage('MAVEN_BUILD') {
+				bat 'mvn -f C:\Users\670334\.jenkins\workspace\pipeline-project\pom.xml clean install'
+		}
+	} catch(Exception e) {
 		e.printStackTrace()
 		}
 	}
